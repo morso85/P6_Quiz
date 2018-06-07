2342fc680b14a9164d3f3db5a0c565c45b17c384
alvarake <alvarocepeda2@gmail.com>
2018-06-07 17:38:13 +0200

    Intento numero 2 despues del merge

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const multer  = require('multer');
-const upload = multer({ dest: './uploads/' });
-
 const quizController = require('../controllers/quiz');
 const tipController = require('../controllers/tip');
 const userController = require('../controllers/user');
 const sessionController = require('../controllers/session');
-const favouriteController = require('../controllers/favourite');
 
 //-----------------------------------------------------------
 
@@ -37,10 +33,11 @@ function saveBack(req, res, next) {
 // Restoration routes are GET routes that do not end in:
 //   /new, /edit, /play, /check, /session, or /:id.
 router.get([
-'/','/author',
-'/users',
-'/users/:id(\\d+)/quizzes',
-'/quizzes'],                 saveBack);
+    '/',
+    '/author',
+    '/users',
+    '/users/:id(\\d+)/quizzes',
+    '/quizzes'], saveBack);
 
 //-----------------------------------------------------------
 
@@ -91,23 +88,21 @@ router.delete('/users/:userId(\\d+)',
     sessionController.adminOrMyselfRequired,
 	userController.destroy);
 
-
 router.get('/users/:userId(\\d+)/quizzes',
     sessionController.loginRequired,
     quizController.index);
 
 
 // Routes for the resource /quizzes
-router.get('/quizzes.:format?',
+router.get('/quizzes',
 	quizController.index);
-router.get('/quizzes/:quizId(\\d+).:format?',
+router.get('/quizzes/:quizId(\\d+)',
 	quizController.show);
 router.get('/quizzes/new',
     sessionController.loginRequired,
 	quizController.new);
 router.post('/quizzes',
     sessionController.loginRequired,
-    upload.single('image'),
 	quizController.create);
 router.get('/quizzes/:quizId(\\d+)/edit',
     sessionController.loginRequired,
@@ -116,23 +111,25 @@ router.get('/quizzes/:quizId(\\d+)/edit',
 router.put('/quizzes/:quizId(\\d+)',
     sessionController.loginRequired,
     quizController.adminOrAuthorRequired,
-    upload.single('image'),
 	quizController.update);
 router.delete('/quizzes/:quizId(\\d+)',
     sessionController.loginRequired,
     quizController.adminOrAuthorRequired,
 	quizController.destroy);
 
-router.get('/quizzes/:quizId(\\d+)/play',
-	quizController.play);
-router.get('/quizzes/:quizId(\\d+)/check',
-	quizController.check);
+router.get('/quizzes/:quizId(\\d+)/play',  quizController.play);
+router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
+
+router.get('/quizzes/randomplay', quizController.randomplay);
+router.get('/quizzes/randomcheck/:quizId(\\d+)', quizController.randomcheck);
 
 
 
-router.post('/quizzes/:quizId(\\d+)/tips',     
+
+
+router.post('/quizzes/:quizId(\\d+)/tips',
     sessionController.loginRequired,
-	tipController.create);
+    tipController.create);
 router.put('/quizzes/:quizId(\\d+)/tips/:tipId(\\d+)/accept',
     sessionController.loginRequired,
     quizController.adminOrAuthorRequired,
@@ -141,17 +138,6 @@ router.delete('/quizzes/:quizId(\\d+)/tips/:tipId(\\d+)',
     sessionController.loginRequired,
     quizController.adminOrAuthorRequired,
     tipController.destroy);
-
-
-// Routes for the resource favourites of a user
-router.put('/users/:userId(\\d+)/favourites/:quizId(\\d+)',
-    sessionController.loginRequired,
-    sessionController.adminOrMyselfRequired,
-    favouriteController.add);
-router.delete('/users/:userId(\\d+)/favourites/:quizId(\\d+)',
-    sessionController.loginRequired,
-    sessionController.adminOrMyselfRequired,
-    favouriteController.del);
 
 
 module.exports = router;
